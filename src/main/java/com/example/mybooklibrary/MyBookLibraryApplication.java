a86629d7c34efe8c328d55fb860d9b482c0a789d
BootCampVM1\assc <kocsvin@gmail.com>
2021-11-12 15:21:40 +0100

    Initial commit

---
+++ src/main/java/com/example/mybooklibrary/MyBookLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.mybooklibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBookLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBookLibraryApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/mybooklibrary/controller/BookController.java
@@ -0,0 +1,11 @@
+package com.example.mybooklibrary.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class BookController {
+    ArrayList<String> bookTitles = new ArrayList<String>();
+
+}
