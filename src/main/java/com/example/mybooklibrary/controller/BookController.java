72ee8dd25ebebf6a1a5cd5e75dc6c2830149f440
BootCampVM1\assc <kocsvin@gmail.com>
2021-11-12 16:01:36 +0100

    pridane add Book , get all books , delete books (nefunkce)

--- src/main/java/com/example/mybooklibrary/controller/BookController.java
+++ src/main/java/com/example/mybooklibrary/controller/BookController.java
@@ -1,11 +1,33 @@
 package com.example.mybooklibrary.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
 @RestController
 public class BookController {
     ArrayList<String> bookTitles = new ArrayList<String>();
+
+    @GetMapping(value = "/")
+    public String index() {
+        return "Hi! I am parrot. I repeat what you say.";
+    }
+    @PostMapping(value = "/book")
+    public void addBook(@RequestBody String book) {
+        bookTitles.add(book);
+    }
+    @GetMapping(value = "/allBooks")
+    public ArrayList<String> getBooks() {
+        return bookTitles;
+    }
+
+    @DeleteMapping(value = "/book")
+    public void deleteLearnedWord(@RequestBody String deletedBook) {
+        for (String book : bookTitles) {
+            bookTitles.removeIf( n -> book.equals(deletedBook));
+        }
+            
+    }
+
 
 }
